4fdb1146e2bb58527e7c68687f16d4f28efe09f8
henriquecfarias <henriquecfarias@outlook.com>
2022-05-17 22:02:40 -0300

    Criacao dos relacionamento

--- IngressoMVC/Models/Filme.cs
+++ IngressoMVC/Models/Filme.cs
@@ -5,8 +5,25 @@ using System.Threading.Tasks;
 
 namespace IngressoMVC.Models
 {
-    public class Filme
+    public class Filme : IEntidade
     {
-        
+        public int Id { get ; set ; }
+        public DateTime DataCadastro { get ; set ; }
+        public DateTime DataAlteracao { get ; set ; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string ImageURL { get; set; }
+
+        #region relacionamento
+
+        public int CinemaID { get; set; }
+        public Cinema Cinema { get; set; }
+        public int ProdutorId { get; set; }
+        public Produtor Produtor { get; set; }
+
+        public List<AtorFilme> AtoresFilmes { get; set; }
+        public List<FilmeCategoria> FilmeCategorias { get; set; }
+        #endregion
     }
 }
